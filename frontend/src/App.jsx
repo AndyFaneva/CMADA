90ac392dc8d46e7dd31d63e31e2187011e96e827
root <root@localhost.localdomain>
2025-05-03 14:01:06 +0300

    [dev]:Backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 class="text-3xl font-bold underline text-right">
+    Hello world!
+  </h1>
+  <h1 class="text-4xl font-bold text-left p-8">
+    Tailwind CSS est installé avec succès !
+  </h1>
+  <button class="bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400">
+  Cliquez ici
+</button>
+
+    </>
+  )
+}
+
+export default App
